e0a43283a77d46bf2892b2dd21acb45005b279ba
122-63 <3469@holbertonschool.com>
2022-05-23 23:44:25 -0500

    add jquery

---
+++ 0x0D-JQuery_advanced/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    if (jQuery) {  
+        console.log("jQuery has been loaded correctly");
+    } else {
+        console.log("jQuery has not been loaded correctly");
+    }
+});
+
